a607866d4e8422184051c0842008c16346ea106d
Rahad-Ullah <myemail.abc20@gmail.com>
2024-08-19 23:02:23 +0600

    Modified

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -201,7 +201,7 @@ const Navbar = () => {
               to="/"
               className="inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-base font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50"
             >
-              ই-সেবা
+              ই-সার্ভিস
             </Link>
           </NavigationMenuLink>
 
